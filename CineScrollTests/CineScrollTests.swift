bb6166f7add5c93efccd205898c66a8350407185
AbdoFoda <abdofoda@stud.fci-cu.edu.eg>
2026-05-16 03:16:27 +0200

    Initial Commit

---
+++ CineScrollTests/CineScrollTests.swift
@@ -0,0 +1,19 @@
+//
+//  CineScrollTests.swift
+//  CineScrollTests
+//
+//  Created by Abdulrahman Foda on 16.05.26.
+//
+
+import Testing
+@testable import CineScroll
+
+struct CineScrollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
